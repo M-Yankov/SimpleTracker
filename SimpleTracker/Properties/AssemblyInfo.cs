bb354290294c4a988623438dfedffdb640083790
M-Yankov <mihail.yankov.1337@gmail.com>
2022-02-05 20:54:59 +0200

    Add missing permission in the assembly manifest.

--- SimpleTracker/Properties/AssemblyInfo.cs
+++ SimpleTracker/Properties/AssemblyInfo.cs
@@ -25,6 +25,8 @@ using Android.App;
 [assembly: AssemblyVersion("1.3.2.0")]
 [assembly: AssemblyFileVersion("1.3.2.0")]
 
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
 [assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
 [assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
 [assembly: UsesFeature("android.hardware.location", Required = false)]
